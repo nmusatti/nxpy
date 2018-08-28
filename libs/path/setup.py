73aa6f18d2f47ef52890e047fc84a8e06a522feb
Nicola Musatti <nicola.musatti@gmail.com>
2018-08-28 20:20:33 +0200

    setup.py files added

---
+++ libs/path/setup.py
@@ -0,0 +1,43 @@
+# nxpy.path ------------------------------------------------------------------
+
+# Copyright Nicola Musatti 2018
+# Use, modification, and distribution are subject to the Boost Software
+# License, Version 1.0. (See accompanying file LICENSE.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
+# See http://nxpy.sourceforge.net for library home page. ---------------------
+
+r"""
+Packaging information.
+
+"""
+
+from setuptools import setup
+
+lib_name = 'nxpy.path'
+
+setup(
+    name=lib_name,
+    version="1.0.0",
+    author="Nicola Musatti",
+    author_email="nicola.musatti@gmail.com",
+    description="Wrap programs to call them as Python methods",
+    license="Boost Software License version 1.0",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+    ],
+    namespace_packages=['nxpy','nxpy.core'],
+    packages=['nxpy.core.path'],
+    install_requires=[
+    ],
+)
---
+++ libs/temp_file/setup.py
@@ -0,0 +1,46 @@
+# nxpy.temp_file -------------------------------------------------------------
+
+# Copyright Nicola Musatti 2018
+# Use, modification, and distribution are subject to the Boost Software
+# License, Version 1.0. (See accompanying file LICENSE.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
+# See http://nxpy.sourceforge.net for library home page. ---------------------
+
+r"""
+Packaging information.
+
+"""
+
+from setuptools import setup
+
+lib_name = 'nxpy.temp_file'
+
+setup(
+    name=lib_name,
+    version="1.0.0",
+    author="Nicola Musatti",
+    author_email="nicola.musatti@gmail.com",
+    description="Temporary file and directory handling",
+    license="Boost Software License version 1.0",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+    ],
+    namespace_packages=['nxpy','nxpy.core'],
+    packages=['nxpy.core.temp_file'],
+    install_requires=[
+        'nxpy.file_object',
+        'nxpy.past',
+        'nxpy.path',
+    ],
+)
