8810b2cab79a61a610bd200d0f6f8a3077b748fa
Nicola Musatti <nicola.musatti@gmail.com>
2019-01-19 17:56:15 +0100

    Corrected copyright notices

--- libs/xml/nxpy/xml/util.py
+++ libs/xml/nxpy/xml/util.py
@@ -1,6 +1,6 @@
 # nxpy_xml --------------------------------------------------------------------
 
-# Copyright Nicola Musatti 2017 - 2018
+# Copyright Nicola Musatti 2017 - 2019
 # Use, modification, and distribution are subject to the Boost Software
 # License, Version 1.0. (See accompanying file LICENSE.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
