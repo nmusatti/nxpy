74f9a6536a94dce131d2cd3f72c37e7f8ec9404c
Nicola Musatti <nicola.musatti@gmail.com>
2020-10-16 19:29:50 +0200

    Corrected release dates.

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 
 # nxpy documentation ----------------------------------------------------------
 
-# Copyright Nicola Musatti 2010 - 2019
+# Copyright Nicola Musatti 2010 - 2020
 # Use, modification, and distribution are subject to the Boost Software
 # License, Version 1.0. (See accompanying file LICENSE.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
